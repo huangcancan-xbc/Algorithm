b4b03928a25c449ae203eac093494b890d2b46a7
小米里的大麦 <hc1960074081@qq.com>
2024-11-18 12:58:53 +0800

    力扣

---
+++ 优选算法/专题五：位运算/00专题前瞻——简单练手/01力扣：191. 位1的个数.cpp
@@ -0,0 +1,23 @@
+﻿//力扣：191. 位1的个数
+//https://leetcode.cn/problems/number-of-1-bits/description/
+
+
+class Solution {
+public:
+    int hammingWeight(int n) 
+    {
+        int ret = 0;                    // 计数器，用于记录 1 的个数
+
+        // 遍历 n 的每一位
+        for (int i = 0; i < 32; i++) 
+        {
+            // 检查第 i 位是否为 1
+            if ((n >> i) & 1) 
+            {
+                ret++;                  // 如果是 1，计数器加 1
+            }
+        }
+
+        return ret;
+    }
+};
