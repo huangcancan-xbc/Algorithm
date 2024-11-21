a795aad08d7576d7ca5466bbb0b580ae1f91b659
小米里的大麦 <hc1960074081@qq.com>
2024-11-21 10:55:39 +0800

    力扣

---
+++ 优选算法/专题五：位运算/00专题前瞻——简单练手/04力扣：136. 只出现一次的数字.cpp
@@ -0,0 +1,20 @@
+﻿//力扣：136. 只出现一次的数字
+//https://leetcode.cn/problems/single-number/description/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret = 0;
+
+        // 使用位运算 XOR（异或）来找到只出现一次的数字
+        for (auto x : nums) 
+        {
+            ret ^= x; 
+            
+            // 每个元素与ret进行异或运算，相同数字异或会抵消 (例如：a ^ a = 0)
+            // 因此所有成对出现的数字会被抵消，剩下单个数字
+        }
+
+        return ret;
+    }
+};
