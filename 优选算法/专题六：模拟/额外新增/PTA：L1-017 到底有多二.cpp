2399d40a0455587b602457e37fd7bd43ada6711f
小米里的大麦 <hc1960074081@qq.com>
2025-05-01 20:08:15 +0800

    PTA

---
+++ 优选算法/专题六：模拟/额外新增/PTA：L1-017 到底有多二.cpp
@@ -0,0 +1,41 @@
+﻿// PTA：L1-017 到底有多二
+// 链接：https://pintia.cn/problem-sets/994805046380707840/exam/problems/type/7?problemSetProblemId=994805121500692480&page=0
+
+// 方法：模拟 + 字符串
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	int n = s.size();
+	double count = 0;
+
+	for (auto x : s)
+	{
+		if (x == '2')
+		{
+			count++;
+		}
+	}
+
+	double fu = 0, ou = 0;
+	if (s[0] == '-')
+	{
+		fu = 0.5;
+		n--;
+	}
+	if ((s[s.size() - 1] - '0') % 2 == 0)
+	{
+		ou = 1;
+	}
+
+	double ret = count / n * (1 + fu) * (1 + ou) * 100;
+
+	printf("%.2f%%\n", ret);
+
+	return 0;
+}
