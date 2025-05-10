a121f13c5b19858aba1bd153650b66d862a80d00
小米里的大麦 <hc1960074081@qq.com>
2025-05-10 08:22:54 +0800

    力扣

---
+++ 优选算法/专题十一：字符串/经典：力扣：344. 反转字符串.cpp
@@ -0,0 +1,29 @@
+﻿//力扣：344. 反转字符串
+//https://leetcode.cn/problems/reverse-string/description/
+
+
+//方法一：库函数reverse()
+//时间复杂度：O(n)
+class Solution {
+public:
+    void reverseString(vector<char>& s) 
+    {
+        reverse(s.begin(), s.end());
+    }
+};
+
+
+
+//方法二：双指针
+//时间复杂度：O(n)
+class Solution {
+public:
+    void reverseString(vector<char>& s) 
+    {
+        int left = 0, right = s.size() - 1;         //左指针 left 指向字符串的第一个字符，右指针 right 指向字符串的最后一个字符
+        while (left < right)
+        {
+            swap(s[left++], s[right--]);            //交换左右指针所指向的字符，并将左指针向右移动，右指针向左移动
+        }
+    }
+};
