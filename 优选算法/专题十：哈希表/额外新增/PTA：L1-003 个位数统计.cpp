66bba38bc1aea69be91bf187ec35066deda9ca15
小米里的大麦 <hc1960074081@qq.com>
2025-05-11 11:04:52 +0800

    PTA

---
+++ 优选算法/专题十：哈希表/额外新增/PTA：L1-003 个位数统计.cpp
@@ -0,0 +1,30 @@
+﻿// PTA：L1-003 个位数统计
+// 链接：https://pintia.cn/problem-sets/994805046380707840/exam/problems/type/7?problemSetProblemId=994805143738892288
+
+// 方法：哈希表
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	int hash[10] = { 0 };
+	for (auto x : s)
+	{
+		hash[x - '0']++;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (hash[i] > 0)
+		{
+			cout << i << ":" << hash[i] << endl;
+		}
+		
+	}
+
+	return 0;
+}
