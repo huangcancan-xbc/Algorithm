54d5c145571b62d9d2a0ef54d12f64bfb3e6a187
小米里的大麦 <hc1960074081@qq.com>
2025-06-22 11:35:38 +0800

    力扣

---
+++ 蓝桥杯真题打卡/day 001/1力扣：LCR 018. 验证回文串.cpp
@@ -0,0 +1,56 @@
+﻿//力扣：LCR 018. 验证回文串
+//链接：https://leetcode.cn/problems/XltzEq/description/
+
+
+//方法：字符串处理
+class Solution 
+{
+public:
+    bool isPalindrome(string s) 
+    {
+        string ss;
+
+        // 过滤非字母数字字符
+        for (auto x : s)
+        {
+            if (x >= 'a' && x <= 'z')
+            {
+                ss += x;
+            }
+            if (x >= 'A' && x <= 'Z')
+            {
+                ss += tolower(x);
+            }
+            if (x >= '0' && x <= '9')
+            {
+                ss += x;
+            }
+        }
+
+        string tmp(ss.rbegin(), ss.rend());         // 反转字符串
+        return tmp == ss;                           // 判断是否回文
+    }
+};
+
+
+
+class Solution 
+{
+public:
+    bool isPalindrome(string s)
+    {
+        string ss;
+
+        // 过滤非字母数字字符
+        for (auto x : s)
+        {
+            if (isalnum(x))
+            {
+                ss += tolower(x);
+            }
+        }
+
+        string tmp(ss.rbegin(), ss.rend());         // 反转字符串
+        return tmp == ss;                           // 判断是否回文
+    }
+};
