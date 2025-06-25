da511dd8cb4d193ed6f3f321c94d7b43b15317b5
小米里的大麦 <hc1960074081@qq.com>
2025-06-25 14:09:51 +0800

    洛谷

---
+++ 蓝桥杯真题打卡/day 004/1洛谷：P11041 [蓝桥杯 2024 省 Java B] 报数游戏.cpp
@@ -0,0 +1,14 @@
+﻿//洛谷：P11041 [蓝桥杯 2024 省 Java B] 报数游戏
+//链接：https://www.luogu.com.cn/problem/P11041
+
+
+//方法：数学模拟
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long n = 202420242024 / 2;
+	cout << 24 * n;
+
+	return 0;
+}
