6532fd1cde8d1e0ea4154295e31cc955f5c8ca98
huangcancan-xbc <hc1960074081@qq.com>
2026-02-01 21:46:33 +0800

    力扣

---
+++ 优选算法/专题九：链表/额外新增/力扣：206. 反转链表.cpp
@@ -0,0 +1,31 @@
+﻿// 力扣：206. 反转链表
+// 链接：https://leetcode.cn/problems/reverse-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        ListNode* prev = nullptr, * cur = head, * temp;
+
+        while (cur)
+        {
+            temp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = temp;
+        }
+
+        return prev;
+    }
+};
