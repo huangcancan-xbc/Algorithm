c591da474933b090f99b3e90e0a5f1cef4ac9e03
huangcancan-xbc <hc1960074081@qq.com>
2026-02-02 00:26:19 +0800

    力扣

---
+++ 优选算法/专题十四：优先级队列(堆)/额外新增/力扣：215. 数组中的第K个最大元素.cpp
@@ -0,0 +1,43 @@
+﻿// 力扣：215. 数组中的第K个最大元素
+// 链接：https://leetcode.cn/problems/kth-largest-element-in-an-array/description/
+
+// 方法一：堆
+class Solution
+{
+public:
+    int findKthLargest(vector<int>& nums, int k)
+    {
+        priority_queue<int> q(nums.begin(), nums.end());
+        while (--k) // 后置--会多弹出一个值，导致错误！
+        {
+            q.pop();
+        }
+
+        return q.top();
+    }
+};
+
+
+
+// 方法二：快排
+class Solution
+{
+public:
+    int findKthLargest(vector<int>& nums, int k)
+    {
+        sort(nums.begin(), nums.end(), greater<int>());
+
+        return nums[k - 1];
+    }
+};
+
+class Solution
+{
+public:
+    int findKthLargest(vector<int>& nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+
+        return nums[nums.size() - k];
+    }
+};
