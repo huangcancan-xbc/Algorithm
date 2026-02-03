c951a31a0dcce012092e88b8f0523f11e2541007
huangcancan-xbc <hc1960074081@qq.com>
2026-02-03 21:57:42 +0800

    力扣

---
+++ 优选算法/专题三：二分查找算法/额外新增/力扣：15. 三数之和.cpp
@@ -0,0 +1,46 @@
+﻿// 力扣：15. 三数之和
+// 链接：https://leetcode.cn/problems/3sum/description/
+
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ret;
+
+        for (int i = 0; i < nums.size() - 2; ++i)
+        {
+            if (nums[i] > 0)
+            {
+                break;
+            }
+            if (i && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+
+            int l = i + 1, r = nums.size() - 1;
+            while (l < r)
+            {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0)
+                {
+                    r--;
+                }
+                else if (sum < 0)
+                {
+                    l++;
+                }
+                else
+                {
+                    ret.push_back({ nums[i], nums[l], nums[r] });
+                    while (l < r && nums[l] == nums[++l]);
+                    while (l < r && nums[r] == nums[--r]);
+                }
+            }
+        }
+
+        return ret;
+    }
+};
