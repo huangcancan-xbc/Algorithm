6876d72697fe371b44d60369790c5df48911454b
huangcancan-xbc <hc1960074081@qq.com>
2026-03-08 21:54:36 +0800

    牛客

---
+++ 优选算法/专题六：模拟/额外新增/牛客：ICPC Medal.cpp
@@ -0,0 +1,32 @@
+﻿// 牛客：ICPC Medal
+// 链接：https://ac.nowcoder.com/acm/contest/129231/B
+
+// 方法：模拟
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long a, b, c, x, y;
+    cin >> a >> b >> c >> x >> y;
+
+    long long j = a;    // 金
+    long long yy = b;    // 银
+    long long t = c;    // 铜
+
+    while (t >= x || yy >= y)
+    {
+        // 铜牌合成银牌
+        yy += t / x;
+        t %= x;
+
+        // 银牌合成金牌
+        long long v1 = yy / y;
+        j += v1;
+        t += v1;  // 合成金牌掉落铜牌
+        yy %= y;
+    }
+
+    cout << j << endl;
+    return 0;
+}
