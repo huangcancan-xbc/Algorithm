9b477aa4579dda0b844b9a7bc8c9afc191308eb3
huangcancan-xbc <hc1960074081@qq.com>
2026-03-24 19:27:46 +0800

    力扣

---
+++ 优选算法/专题七：分治 - 快速排序/额外新增/力扣：274. H 指数.cpp
@@ -0,0 +1,28 @@
+﻿// 力扣：274. H 指数
+// 链接：https://leetcode.cn/problems/h-index/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 方法：排序
+// 对于题意我感觉非常模糊，有点难理解，但是可以隐约感觉到：
+// 想让我们找一个数，这个目标数要保证数组中大于这个目标数的值的个数等于这个目标数
+// 感觉有点抽象了……
+class Solution
+{
+public:
+    int hIndex(vector<int>& citations)
+    {
+        sort(citations.begin(), citations.end(), greater<int>());
+
+        int ret = 0;
+        for (int i = 0; i < citations.size(); i++)
+        {
+            if (citations[i] < i + 1)
+            {
+                break;
+            }
+
+            ret++;
+        }
+
+        return ret;
+    }
+};
