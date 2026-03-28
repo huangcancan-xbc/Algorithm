9179ccde8be013b33eaca74eba57b24b9a7d9cb3
huangcancan-xbc <hc1960074081@qq.com>
2026-03-28 16:03:49 +0800

    PTA

---
+++ 优选算法/专题六：模拟/额外新增/PTA：L1-111 大幂数.cpp
@@ -0,0 +1,38 @@
+﻿// PTA：L1-111 大幂数
+// 链接：https://pintia.cn/problem-sets/994805046380707840/exam/problems/type/7?problemSetProblemId=1913922872972247046&page=1
+
+// 方法：暴力枚举
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    long x;
+    cin >> x;
+
+    for (int i = 30; i >= 1; i--)
+    {
+        long sum = 0;
+        for (int j = 1; j < 100000; j++)
+        {
+            sum += pow(j, i);
+            if (sum == x)
+            {
+                for (int k = 1; k <= j; k++)
+                {
+                    cout << k << "^" << i;
+                    if (k != j)
+                    {
+                        cout << "+";
+                    }
+                }
+
+                return 0;
+            }
+        }
+    }
+
+    cout << "Impossible for " << x << "." << endl;
+    return 0;
+}
