cbb556ba909327601cc6280ab29480ec4b4aea74
huangcancan-xbc <hc1960074081@qq.com>
2026-04-04 22:43:35 +0800

    力扣

---
+++ 优选算法/专题一：双指针/额外新增/力扣：125. 验证回文串.cpp
@@ -0,0 +1,104 @@
+﻿// 力扣：125. 验证回文串
+// 链接：https://leetcode.cn/problems/valid-palindrome/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 方法一：字符串硬模拟 + 转换
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        string ss;
+        for (auto x : s)
+        {
+            if (x >= 'A' && x <= 'Z')
+            {
+                ss += x + 32;
+            }
+            else if ((x >= 'a' && x <= 'z') || (x >= '0' && x <= '9'))
+            {
+                ss += x;
+            }
+        }
+
+        string sss = ss;
+        reverse(sss.begin(), sss.end());
+        return sss == ss;
+    }
+};
+
+
+
+
+// 方法二：使用库函数
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        string ss;
+        for (auto x : s)
+        {
+            if (x >= 'A' && x <= 'Z')
+            {
+                ss += tolower(x);
+            }
+            else if (x >= 'a' && x <= 'z')
+            {
+                ss += x;
+            }
+            else if (x >= '0' && x <= '9')
+            {
+                ss += x;
+            }
+        }
+
+        string sss = ss;
+        reverse(sss.begin(), sss.end());
+        return sss == ss;
+    }
+};
+
+
+
+
+// 方法三：纯手动模拟（双指针）
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        int i = 0, j = s.size() - 1;
+        while (i < j)
+        {
+            while (i < j && !isalnum(s[i]))
+            {
+                i++;        // 左边跳过无效字符
+            }
+            while (i < j && !isalnum(s[j]))
+            {
+                j--;        // 右边跳过无效字符
+            }
+
+            char left = s[i], right = s[j];
+
+            if (left >= 'A' && left <= 'Z')
+            {
+                left += 32;
+            }
+            if (right >= 'A' && right <= 'Z')
+            {
+                right += 32;
+            }
+
+            if (left != right)
+            {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
