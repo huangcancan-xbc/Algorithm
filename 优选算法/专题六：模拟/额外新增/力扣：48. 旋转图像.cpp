92f4735d4e50d7340b5782f02612821be2f3fa7f
huangcancan-xbc <hc1960074081@qq.com>
2026-04-06 16:42:08 +0800

    力扣

---
+++ 优选算法/专题六：模拟/额外新增/力扣：48. 旋转图像.cpp
@@ -0,0 +1,54 @@
+﻿// 力扣：48. 旋转图像
+// 链接：https://leetcode.cn/problems/rotate-image/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 方法：模拟 + 找规律
+class Solution
+{
+public:
+    void rotate(vector<vector<int>>& matrix)
+    {
+        int n = matrix.size();
+
+        // 先沿对角线翻转
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (i != j)
+                {
+                    swap(matrix[i][j], matrix[j][i]);
+                }
+            }
+        }
+
+        // 在水平翻转
+        for (int i = 0; i < n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+
+
+
+
+// 方法二：数学模拟定位
+class Solution
+{
+public:
+    void rotate(vector<vector<int>>& matrix)
+    {
+        int n = matrix.size();
+        vector<vector<int>> temp(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                temp[j][n - 1 - i] = matrix[i][j];  // 直接算出目标位置
+            }
+        }
+
+        matrix = temp;
+    }
+};
