388685f6363d77057366546f40478465a2282881
huangcancan-xbc <hc1960074081@qq.com>
2026-04-13 19:26:21 +0800

    力扣

---
+++ 优选算法/专题十：哈希表/额外新增/力扣：383. 赎金信.cpp
@@ -0,0 +1,63 @@
+﻿// 力扣：383. 赎金信
+// 链接：https://leetcode.cn/problems/ransom-note/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 方法：哈希
+class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        if (magazine.size() < ransomNote.size())
+        {
+            return false;
+        }
+
+        unordered_map<char, int> hash;
+        for (auto& x : magazine)
+        {
+            hash[x]++;
+        }
+
+        for (auto& x : ransomNote)
+        {
+            if (!hash.count(x) || --hash[x] < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+
+
+// 数组优化：
+class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        if (magazine.size() < ransomNote.size())
+        {
+            return false;
+        }
+
+        int hash[26] = { 0 };
+        for (char c : magazine)
+        {
+            hash[c - 'a']++;
+        }
+
+        for (char c : ransomNote)
+        {
+            if (--hash[c - 'a'] < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
