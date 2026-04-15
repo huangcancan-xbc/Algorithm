205318a6b33f54bd91a60c015c2d8aafa2c675a5
huangcancan-xbc <hc1960074081@qq.com>
2026-04-15 23:35:02 +0800

    力扣

---
+++ 优选算法/专题十：哈希表/额外新增/力扣：205. 同构字符串.cpp
@@ -0,0 +1,39 @@
+﻿// 力扣：205. 同构字符串
+// 链接：https://leetcode.cn/problems/isomorphic-strings/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 方法：哈希
+class Solution
+{
+public:
+    bool isIsomorphic(string s, string t)
+    {
+        if (s.size() != t.size())
+        {
+            return false;
+        }
+
+        unordered_map<char, char> hash1, hash2;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            //if ((hash1.count(s[i]) && hash2[t[i]] != s[i]) || (hash2.count(t[i]) && hash1[s[i]] != t[i]))
+            //{
+            //    return false;
+            //}
+            if (hash1.count(s[i]) && hash2[t[i]] != s[i])
+            {
+                return false;
+            }
+
+            if (hash2.count(t[i]) && hash1[s[i]] != t[i])
+            {
+                return false;
+            }
+
+            hash1[s[i]] = t[i];
+            hash2[t[i]] = s[i];
+        }
+
+        return true;
+    }
+};
