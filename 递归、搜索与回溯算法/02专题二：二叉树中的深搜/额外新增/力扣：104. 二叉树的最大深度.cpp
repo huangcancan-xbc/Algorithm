1f9c51fd89e3224838be8baffdc8ff2b17b34f79
huangcancan-xbc <hc1960074081@qq.com>
2026-04-27 23:28:02 +0800

    力扣

---
+++ 递归、搜索与回溯算法/02专题二：二叉树中的深搜/额外新增/力扣：104. 二叉树的最大深度.cpp
@@ -0,0 +1,28 @@
+﻿// 力扣：104. 二叉树的最大深度
+// 链接：https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 方法：DFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxDepth(TreeNode* root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
