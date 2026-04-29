2641869bbde0641d69773f1e268413c034f0a1f3
huangcancan-xbc <hc1960074081@qq.com>
2026-04-29 23:37:45 +0800

    力扣

---
+++ 优选算法/专题十一：字符串/额外新增/力扣：9. 回文数.cpp
@@ -0,0 +1,21 @@
+﻿// 力扣：9. 回文数
+// 链接：https://leetcode.cn/problems/palindrome-number/description/?envType=study-plan-v2&envId=top-interview-150
+
+// 方法：字符串
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        string s = to_string(x);
+        if (s[0] == '-' || s[0] == '+')
+        {
+            return false;
+        }
+
+        string ss = s;
+        reverse(ss.begin(), ss.end());
+
+        return ss == s;
+    }
+};
